8618a154c4c1bc21f1fff23964540912d3c84e74
Gemma Taylor <gemmaktaylor@msn.com>
2024-07-25 17:55:08 +0100

    complied js functions into one change content function

--- navigation.js
+++ navigation.js
@@ -1,14 +1,16 @@
-const nav = document.querySelector(".primary-navigation");
-const navBtn = document.querySelector(".mobile-nav-toggle");
-
-navBtn.addEventListener("click", () => {
-    const navVisible = nav.getAttribute("data-visible");
-
-    if (navVisible === "false") {
-        nav.setAttribute("data-visible", "true");
-        navBtn.setAttribute("aria-expanded", "true");
-    } else {
-        nav.setAttribute("data-visible", "false");
-        navBtn.setAttribute("aria-expanded", "false");
-    }
-});
+export default function navigation() {
+    const nav = document.querySelector(".primary-navigation");
+    const navBtn = document.querySelector(".mobile-nav-toggle");
+    
+    navBtn.onclick = () => {
+        const navVisible = nav.getAttribute("data-visible");
+    
+        if (navVisible === "false") {
+            nav.setAttribute("data-visible", "true");
+            navBtn.setAttribute("aria-expanded", "true");
+        } else {
+            nav.setAttribute("data-visible", "false");
+            navBtn.setAttribute("aria-expanded", "false");
+        }
+    };
+}
